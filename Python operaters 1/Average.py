ac48f3cfcb813a407d031af90efbbd02927baac3
Harshusen7 <eashaimpex@gmail.com>
2025-12-03 17:39:23 -0800

    Python operater 1

---
+++ Python operaters 1/Average.py
@@ -0,0 +1,12 @@
+# Storing Values
+tree1 = 77
+tree2 = 56
+tree3 = 89
+tree4 = 19
+tree5 = 23
+# Finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sumof all trees is:", sum)
+# Finding the average of trees
+average = sum/5
+print("the average of all trees is:", average)
