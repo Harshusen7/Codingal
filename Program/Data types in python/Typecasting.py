3455bf53452df95178a2bac3de2f22c50a4cd03e
Harshusen7 <eashaimpex@gmail.com>
2025-12-03 20:41:57 -0800

    Python operaters 1

---
+++ Program/Data types in python/Typecasting.py
@@ -0,0 +1,26 @@
+name = "penguin"
+age = 15
+is_student = True
+Weight = 38.5
+# Printing Different variables and their data type
+print("name :", name)
+print("Data type of name is", type(name))
+print("age :", age)
+print("Data type of age is", type(age))
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+print("Weight :", Weight)
+print("Data type of  Weight is", type( Weight))
+# Type casting to convert the datatype of variables
+print("/n After Type casting....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+Weight = int(Weight)
+print(Weight)
+print("Data type of  Weight is", type( Weight))
+
+
+
+
+
