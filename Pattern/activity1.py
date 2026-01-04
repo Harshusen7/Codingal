ba8d44895e2b9661a1388e547676042eb4d57772
Harshusen7 <eashaimpex@gmail.com>
2026-01-04 10:07:32 -0800

    pattern

---
+++ Pattern/activity1.py
@@ -0,0 +1,10 @@
+#Take input
+print("Half pyramid of stars (*):")
+n= int(input("enter the number of rows: "))
+#outer loop to handle number of rows
+for i in range(n):
+   #inner loop to handle number of colums
+    for j in range(i+1):
+            #display result
+            print("*", end="")
+    print()
