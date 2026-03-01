1205132a166f318de93dcba198c36aed98397c50
Harshusen7 <eashaimpex@gmail.com>
2026-03-01 09:56:26 -0800

    list activity

---
+++ lists/activity1.py
@@ -0,0 +1,16 @@
+#Create an empty list
+empty_list = []
+print(empty_list)
+
+# A different data type
+numbers = ['a', 2, 'hello', 4, 5]
+print(numbers)
+
+# Use * operator
+triples = [1, 2, 3] * 3
+print(triples)
+
+#reverse the given list
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList,"\n")
