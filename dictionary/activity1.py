f77c5dead1de2c7e7397051123b0951699ec7b46
Harshusen7 <eashaimpex@gmail.com>
2026-03-08 10:55:21 -0700

    activity 1

---
+++ dictionary/activity1.py
@@ -0,0 +1,48 @@
+student_data = {'id1':
+    {'name' : ['sara'],
+     'class' : ['v'],
+     'subject_intergration': ['english, maths, science']
+    },
+  'id2':
+    {'name' : ['David'],
+     'class' : ['v'],
+     'subject_intergration': ['english, maths, science']
+    },
+   'id3':
+    {'name' : ['sara'],
+     'class' : ['v'],
+     'subject_intergration': ['english, maths, science']
+    },
+   'id4':
+    {'name' : ['surya'],
+     'class' : ['v'],
+     'subject_intergration': ['english, maths, science']
+    },
+}
+
+result = {}  
+
+for key,value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
