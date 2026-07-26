f417f6a90f9596fc6db363162f0f3bb67294dabb
Harshusen7 <eashaimpex@gmail.com>
2026-07-26 11:09:24 -0700

    app

---
+++ Geometry manager/login_app.py
@@ -0,0 +1,13 @@
+# Import necessary libraries
+from tkinter import *
+
+# create window
+root = Tk()
+root.title('Login App')
+root.geometry('400x400')
+
+# create a frame to organize elements better
+frame = Frame(master=root, height=200, width=360, bg= "#d0efff")
+
+# add widgets
+# add Label
