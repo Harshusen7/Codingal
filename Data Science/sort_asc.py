b4c39dd8be029b740cf5a9f7bd13fa8180561e70
Harshusen7 <eashaimpex@gmail.com>
2026-09-06 11:08:24 -0700

    sort

---
+++ Data Science/sort_asc.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+data_type = [('name', 'S15'), ('class', int), ('haight', float)]
+students_details = [('James', 5, 48.5), ('Nail', 6, 52.5), ('Paul', 5, 42.10), ('Pit', 5, 40.11)]
+
+# create a structured array
+students = np.array(students_details, dtype=data_type)
