071a494c26cadacdfaaebde5388144f77fedb2c7
Harshusen7 <eashaimpex@gmail.com>
2026-09-12 11:13:07 -0700

    gaming

--- Data Science/sort_asc.py
+++ Data Science/sort_asc.py
@@ -5,3 +5,4 @@ students_details = [('James', 5, 48.5), ('Nail', 6, 52.5), ('Paul', 5, 42.10), (
 
 # create a structured array
 students = np.array(students_details, dtype=data_type)
+data_type = [(name = )]
---
+++ Gaming leader board analyser/gaming_leaderboard_analyser.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# PART 1 - Create a pandas series of top player scores
+print('--- PART 1. Pandas series ---')
+scores = [98500, 87200, 76400, 65100, 54800]
+players = pd.Series(scores,index=['NightWolf', 'StarBlaze', 'PixelKing', 'Cyberfox', 'IronStorms'])
+print(players)
+
+
+print()
+print('--- PART 2: Pandas DataFrame ---')
+data = {
+    'Player':['NightWolf', 'StarBlaze', 'PixelKing', 'Cyberfox', 'IronStorms'],
+    'Level': [42, 38, 35, 30, 27],
+    'Score': [98500, 87200, 76400, 65100, 54800],
+    'Wins': [210, 185, 162, 140, 118]
+}
+df = pd.DataFrame(data)
+print(df)
