5a9784ea9c4c66ad4061c4fd06e098433a2f8202
Harshusen7 <eashaimpex@gmail.com>
2026-09-13 11:04:25 -0700

    score tracker

---
+++ Matplotlib/my_score_tracker.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+days   =['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+scores = [70, 75, 80, 85, 90]
+
+plt.plot(days,scores)
+plt.show()
+
+plt.plot(days, scores)
+plt.title('My quiz score tracker')
+plt.xlabel('Day of the week')
+plt.ylabel('Score')
+plt.grid(True)
+plt.ylim(0,100)
+plt.show()
+
+plt.plot(days, scores, color='Blue', marker='o', linestyle='dashed',linewidth=2)
+plt.title('My quiz score tracker')
+plt.xlabel('Day of the week')
+plt.ylabel('Score')
+plt.grid(True)
+plt.ylim(0,100)
+plt.show()
+
+plt.bar(days, scores, color='Orange')
+plt.title('My quiz score bar Chart')
+plt.xlabel('Day of the week')
+plt.ylabel('score')
+plt.ylim(0, 100)
+plt.show()
